9975216d003a7d5221640a4dbd3de1b32aa285fb
unknown <subaramms7648@gmail.com>
2025-01-20 16:58:05 +0530

    Added Services to Service class

--- src/main/java/com/practice/Student/service/StudentService.java
+++ src/main/java/com/practice/Student/service/StudentService.java
@@ -1,5 +1,35 @@
 package com.practice.Student.service;
 
-public class StudentService {
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.practice.Student.model.Student;
+import com.practice.Student.repository.StudentRepository;
+
+@Service
+public class StudentService {
+	
+	@Autowired
+	private StudentRepository studentRepo;
+	
+	public Student addStudent(Student student)
+	{
+		return studentRepo.save(student);
+	}
+	
+	public List<Student> getAllStudents()
+	{
+		return studentRepo.findAll();
+	}
+	public void deleteStudent(long id)
+	{
+		studentRepo.deleteById(id);
+	}
+	public Student updateStudent(Student student)
+	{
+		return studentRepo.save(student);
+	}
 }
