504816b86e6a5509bd870f0b111c319dafb0d5d4
Sanjug16 <135541846+Sanjug16@users.noreply.github.com>
2025-01-20 15:59:12 +0530

    Student REpository Created.

--- src/main/java/com/practice/Student/model/Student.java
+++ src/main/java/com/practice/Student/model/Student.java
@@ -1,6 +1,9 @@
 package com.practice.Student.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,7 +12,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@Table(name = "student_details")
 public class Student {
+    @Id
+    @Column(name = "student_id")
     private Long studentId;
 
     private String Name;
